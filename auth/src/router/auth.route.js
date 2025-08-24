62cbf7282efc5495dffa5c62f27e978f4ccbea98
Prince Sharma <hit.princeraj@gmail.com>
2025-08-25 01:42:34 +0530

    fix: correct spelling of 'password' in registration and login; improve error handling

--- auth/src/router/auth.route.js
+++ auth/src/router/auth.route.js
@@ -5,47 +5,72 @@ const jwt = require('jsonwebtoken')
 
 //register
 authRoute.post('/register', async (req, res) => {
-  const { username, passward } = req.body
+  try {
+    const { username, password } = req.body
+    const user = await userModal.create({
+      username, password
+    })
+    const token = jwt.sign({
+      id: user._id,
+    }, process.env.JWT_SECRET)
 
-  const user = await userModal.create({
-    username, passward
-  })
-
-  const token = jwt.sign({
-    id: user._id,
-
-  }, process.env.JWT_SECRET)
-
-
-  res.status(201).json({
-    message: "user registerd",
-    user,
-    token
-  })
+    res.status(201).json({
+      message: "user registerd",
+      user,
+      token
+    })
+  } catch (error) {
+    res.status(500).json({ message: "server error", error })
+  }
 })
 
-//login
+//Login
 authRoute.post('/login', async (req, res) => {
-  const { username, passward } = req.body
-  const isUserExist = await userModal.findOne({
-    username: username
-  })
+  try {
+    const { username, password } = req.body
+    const isUserExist = await userModal.findOne({
+      username: username
+    })
+    if (!isUserExist) {
+      return res.status(401).json({
+        message: "user account not found [invalid username]"
+      })
+    }
+    const isPasswardValid = password == isUserExist.password
+    if (!isPasswardValid) {
+      res.status(401).json({
+        message: "password invalid"
+      })
+    }
+    res.status(201).json({
+      message: "user login"
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: "server error", error
+    })
+  }
+})
 
-  if (!isUserExist) {
-    return res.status(401).json({
-      message: "user account not found [invalid username]"
+//user
+
+authRoute.get('/user', async (req, res) => {
+  try {
+    const { token } = req.body
+
+    if (!token) {
+      return res.status(401).json({
+        message: "unauthorized"
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "server error",
+      error
     })
   }
 
-  const isPasswardValid = passward == isUserExist.passward
-  if (!isPasswardValid) {
-    res.status(401).json({
-      message: "passward invalid"
-    })
-  }
-  res.status(201).json({
-    message: "user login"
-  })
+
 })
 
 module.exports = authRoute
